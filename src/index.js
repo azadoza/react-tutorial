0e2c011f8f7c2ce38cd2a306fa19a22df58bfbb2
aza <azalea.m@pm.me>
2022-06-17 13:50:57 -0500

    boolean flips current player, in render check if there is a winner based on array of possible wins otherwise continue the player flip

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function Square(props) {
   /* all react component class w a constructor start w super(props) */
        
   return(
-        /* squares are controlled components.
+        /* squares are function components.
         they informs board when clicked.
         Board sends value to square */
         <button className="square"
@@ -23,13 +23,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares : Array(9).fill(null),
+      // first making x next default
+      xIsNext: true,
     };
   }
   handleClick(i) {
     /* .slice creates a copy of squares array for immutability */
     const squares = this.state.squares.slice();
-    squares[i] = 'x'
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'x' : 'o';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext});
   }
     renderSquare(i) {
         return(
@@ -40,7 +44,15 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner){
+        status = 'winner ' + winner;
+      }
+      else{
+        status = 'Next player: ' + 
+        (this.state.xIsNext ? 'x' : 'o');
+      }
 
         return (
             <div>
@@ -96,7 +108,25 @@ class Game extends React.Component {
         );
     }
 }
-
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
